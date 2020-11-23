cd208436edd2f00211eb4a955cc955e4a90c8655
Tamara Sirakanyan <tamarasirakanyan@tamsmac.local>
2020-11-23 13:27:12 +0400

    implemented game logic

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const randomNum = Math.trunc((Math.random() * 20) + 1)
+console.log(randomNum);
+const message = document.querySelector('.message')
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guessNum = Number(document.querySelector('.guess').value)
+    console.log(guessNum);
+
+    if (score > 0) {
+        // empty input | number is correct | number is high or low
+        if (!guessNum) {
+            message.textContent = '⛔️ No number!'
+        } else if (guessNum === randomNum) {
+            message.textContent = '🎉 Correct Number!';
+            document.querySelector('.number').textContent = guessNum;
+        } else {
+            guessNum > randomNum ? message.textContent = '📈 Too high!' : message.textContent = '📉 Too low!';
+            score--;
+            document.querySelector('.score').textContent = score
+        }
+    } else {
+        message.textContent = '😕 You lost, sorry!'
+    }
+})
