27f77bdf17da99f6b3f042b491a8cbb1df73485a
Tamara Sirakanyan <tamarasirakanyan@tamsmac.local>
2020-11-24 11:25:05 +0400

    cleanup

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let randomNum = Math.trunc((Math.random() * 20) + 1)
 const message = document.querySelector('.message');
+const number = document.querySelector('.number');
 const body = document.querySelector('body')
 let score = 20;
 let highscore = 0;
@@ -15,8 +16,8 @@ document.querySelector('.check').addEventListener('click', function () {
             message.textContent = '⛔️ No number!';
         } else if (guessNum === randomNum) {
             message.textContent = '🎉 Correct Number!';
-            document.querySelector('.number').textContent = guessNum;
-            document.querySelector('.number').style.width = '30rem';
+            number.textContent = guessNum;
+            number.style.width = '30rem';
             body.style.backgroundColor = '#60b347';
             score > highscore ? highscore = score : null;
             document.querySelector('.highscore').textContent = highscore;
@@ -36,8 +37,8 @@ document.querySelector('.again').addEventListener('click', function () {
     body.style.backgroundColor = '#222';
     score = 20;
     document.querySelector('.score').textContent = '20';
-    document.querySelector('.number').style.width = '15rem';
-    document.querySelector('.number').textContent = '?'
+    number.style.width = '15rem';
+    number.textContent = '?'
     document.querySelector('.guess').value = '';
     randomNum = Math.trunc((Math.random() * 20) + 1);
 })
